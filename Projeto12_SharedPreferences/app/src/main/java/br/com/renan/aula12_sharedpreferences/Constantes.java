250eb8d452012664766cb50ec84cb45f1ddc8b4d
Renan A <renanathea@hotmail.com>
2017-07-17 15:13:36 -0300

    Aula Doze

---
+++ Projeto12_SharedPreferences/app/src/main/java/br/com/renan/aula12_sharedpreferences/Constantes.java
@@ -0,0 +1,11 @@
+package br.com.renan.aula12_sharedpreferences;
+
+/**
+ * Renan de Arimathea
+ * https://github.com/Renanthea
+ */
+
+public class Constantes {
+    public final static String APP_PREFS = "app_prefs";
+    public final static String USERNAME_KEY = "username";
+}
